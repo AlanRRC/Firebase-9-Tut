41ea857910c03183a5528790bce11924e93468e0
Alan Simpson <asimpson@rrc.ca>
2022-05-10 09:53:40 -0500

    firebase app and config added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello from index.js")
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7c8jPsLdVVOA7tAXehUXyvE0qee-zpfY",
+    authDomain: "fir-9-dojo-7d8b2.firebaseapp.com",
+    projectId: "fir-9-dojo-7d8b2",
+    storageBucket: "fir-9-dojo-7d8b2.appspot.com",
+    messagingSenderId: "896338561569",
+    appId: "1:896338561569:web:6500f4f637048f3706f160",
+    measurementId: "G-P0MME3Q0H3"
+}
+
+initializeApp(firebaseConfig)
